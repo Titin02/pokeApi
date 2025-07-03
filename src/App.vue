a9cbac71a4b410db04b48d69f85265cffc7e0241
zanamanzana <sebita.azul@live.cl>
2025-07-03 00:31:22 -0400

    view counter pokemon (modificado app)

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,6 @@
 							Volver
 						</button>
 						<!-- <popUp/> -->
-						<CardCounter/>
 					</div>
 				</div>
 			</div>
@@ -42,7 +41,24 @@
 					:key="pokemon.name"
 					class="flex justify-center"
 				>
+				<div  @click="openPopup(pokemon)">
 					<CardPokemon :pokemonData="pokemon" :generationName="pokemon.generation" />
+				</div>
+				</div>
+			</div>
+			<div v-if="popUpActive" class="popup-overlay active">
+				<div class="popup-content">
+					<div v-if="typeData && typeData.length">
+						<p><strong>Es débil contra:</strong></p>
+						<ul>
+							<li v-for="tipo in typeData" :key="tipo.name">
+							{{ tipo.name }}
+							</li>
+						</ul>
+					</div>
+					<div class="popup-buttons">
+						<button @click="closePopup" class="no-popup">Atras</button>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -53,15 +69,14 @@
 import CardPokemon from "./components/common/CardPokemon.vue";
 import { fetchPokemon } from "./services/searchPokemon.js";
 import { generation } from "./services/generation.js";
+import { TypePokemon } from './services/type.js';
 import searchForm from "./components/search/formSearchPokemon.vue";
-import CardCounter from "./components/common/CardCounter.vue";
 import GenerationSelector from "./components/common/GenerationSelector.vue";
 
 export default {
 	components: {
 		CardPokemon,
 		searchForm,
-		CardCounter,
 		GenerationSelector,
 	},
 	data() {
@@ -93,9 +108,53 @@ export default {
 				{ id: 9, name: "Generación IX"}
 			],
 			isFilterByGeneration: false,
+			popUpActive: false,
+			typeData: null,
 		};
 	},
 	methods: {
+		async openPopup(pokemon) {
+            this.popUpActive = true;
+			this.pokemonData = pokemon;
+			this.loadPokemonType(pokemon)
+        },
+		closePopup() {
+            this.popUpActive = false;
+        },
+		async loadPokemonType(pokemon) {
+			try {
+			const pokemonData = await TypePokemon(pokemon);
+			const typeUrls = pokemonData.types.map(t => t.type.url);
+
+			const allTypes = [];
+
+			for (const url of typeUrls) {
+			const res = await fetch(url);
+			const typeData = await res.json();
+			allTypes.push(typeData.damage_relations.double_damage_from);
+			}
+
+			// aplanamos y eliminamos duplicados por nombre
+			const flat = allTypes.flat();
+			const unique = [...new Map(flat.map(t => [t.name, t])).values()];
+
+			this.typeData = unique;
+			console.log("Es débil contra:", unique.map(t => t.name));
+		} catch (e) {
+			console.error("error al cargar:", e);
+		}
+		// 	console.log("Abriste el popup del:", pokemon.name);
+		// 	const type = await TypePokemon(pokemon);
+		// 	console.log("es debil con:", type);
+		// 	if (!pokemon || !pokemon.name) {
+		// 		throw new Error("El Pokémon no es válido");
+		// 	}
+
+		// 	this.typeData = type; // guarda solo una vez
+		// } catch (e) {
+		// 	console.log("error al cargar:", e);
+		// }
+        },
 		normalizeText(text) {
 			return text
 				.trim()
@@ -111,7 +170,6 @@ export default {
 				this.pokemonSearch = false;
 				return;
 			}
-			console.log(cleanId)
 			const data = await fetchPokemon(cleanId);
 			try {
 				if (data) {
@@ -271,3 +329,92 @@ export default {
 	},
 };
 </script>
+<style scoped>
+.popup-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    display: none;
+    justify-content: center;
+    align-items: center;
+    font-family: sans-serif;
+}
+
+.popup-content {
+    width: auto;
+    max-width: 514px;
+    height: 268px;
+    background: #fff;
+    border-radius: 16px;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    text-align: center;
+    padding: 32px 32px 14px 32px;
+}
+
+.popup-overlay.active {
+    display: flex;
+}
+
+.popup-img {
+    text-align: center;
+}
+
+.popup-message {
+    padding: 32px 28px 0 28px;
+}
+
+.popup-message label {
+    font-size: 24px;
+}
+
+.popup-message p {
+    font-size: 16px;
+}
+
+.no-popup {
+    width: auto;
+    min-width: 80px;
+    height: 36px;
+    border: 1.4px solid #3688b7;
+    color: #3688b7;
+    border-radius: 5px;
+    background: #fff;
+    cursor: pointer;
+    transition: all 0.2s linear;
+    padding: 8px 16px 8px 16px;
+}
+
+.yes-popup {
+    width: auto;
+    min-width: 80px;
+    height: 36px;
+    background: #ff8c2b;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.2s linear;
+    padding: 8px 16px 8px 16px;
+}
+
+.yes-popup:hover {
+    background: #f9ae65;
+    color: #4e4442;
+}
+
+.no-popup:hover {
+    background: #c1dbea;
+    color: #313546;
+}
+
+.popup-buttons {
+    display: flex;
+    padding-top: 32px;
+    justify-content: center;
+    gap: 32px;
+}
+
+</style>
